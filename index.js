a4af1f9c385cc0de587d3c447892f1cee2d76e44
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2024-01-15 23:22:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const auth = require('./routes/auth');
-const cors = require('cors');
-app.use(cors());
 const app = express();
 app.use(express.json());
 if(process.env.NODE_ENV != "production") require('dotenv').config();
